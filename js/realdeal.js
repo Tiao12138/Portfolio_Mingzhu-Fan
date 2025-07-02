09f3c60bade70e42326890c051cd0c963d49a039
Mingzhu <120184562+Tiao12138@users.noreply.github.com>
2025-07-02 23:06:07 +0200

    Update realdeal.js

--- js/realdeal.js
+++ js/realdeal.js
@@ -165,49 +165,6 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.appendChild(script); 
   }
 
-function applyImageClickListeners() {
-        const images = document.querySelectorAll('.cover');
-        const defaultContent = document.querySelector('.contentsec');
-        const contentSections = document.querySelectorAll('[id^="contentsec"]');
-
-        images.forEach(image => {
-            image.addEventListener('click', () => {
-                if (defaultContent) {
-                    defaultContent.classList.add('hidden');
-                }
-
-                contentSections.forEach(section => section.classList.add('hidden'));
-
-                const imageInfo = image.getAttribute('image-info');
-                const contentToShow = document.getElementById(`contentsec${imageInfo.slice(1)}`);
-                if (contentToShow) {
-                    contentToShow.classList.remove('hidden');
-                }
-
-                const backButton = contentToShow.querySelector('img[id^="back"]');
-                if (backButton) {
-                    backButton.addEventListener('click', () => {
-                        defaultContent.classList.remove('hidden');
-                        contentToShow.classList.add('hidden');
-                    }, { once: true }); 
-                }
-            });
-        });
-    }
-
-    function boxlayout() {
-        const row = document.querySelector('.row');
-        const boxes = row.children;
-        if (boxes.length === 1) {
-            row.style.justifyContent = 'flex-start'; 
-        } else {
-            row.style.justifyContent = 'flex-start'; 
-        }
-    }
-
-    applyImageClickListeners();
-    boxlayout();
-});
 
   function showclickme(){
     const col01 = document.querySelector('.col01');
